ae16f73075b00f2253545247143e06afc3c4234a
Wesley <nah>
2015-11-13 20:36:45 -0500

    Adding pid and main code

---
+++ quadcopter/quadcopter.ino
@@ -0,0 +1,13 @@
+#include "pid.h"
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
